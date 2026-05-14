84df9f73b812c66652e574f2fb4639a20f74c5dd
hypergameswebsite <hypergameswebsite@gmail.com>
2026-05-14 06:46:49 +0000

    Make code work with the basics

--- main.js
+++ main.js
@@ -7,7 +7,36 @@ let con = mysql.createConnection({
 });
 
 function run_login() {
-}
+  if (!username || !password) {
+    alert("Please fill in all fields.");
+    return;
+  }
+
+  con.connect(function(err) {
+    if (err) throw err;
+    let sql = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      if (result.length > 0) {
+        alert("Login successful!");
+      } else {
+        alert("Invalid username or password.");
+      }
+    });
+} );};
 
 function run_register() {
-}
+  if (!username || !password) {
+    alert("Please fill in all fields.");
+    return;
+  }
+
+  con.connect(function(err) {
+    if (err) throw err;
+    let sql = `INSERT INTO users (username, password, info_positives, info_negatives, info_house_points) VALUES ('${username}', '${password}', ${info_positives}, ${info_negatives}, ${info_house_points})`;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      alert("Registration successful!");
+    });
+  });
+};                  
